69f9159b2e34fc5771304bb9568d79db96c5a4a2
Matheus Sansonowicz <128191037+MatheusSansonowicz@users.noreply.github.com>
2026-06-12 15:40:22 -0300

    Update firebaseconfig.js

--- configsExports/firebaseconfig.js
+++ configsExports/firebaseconfig.js
@@ -8,13 +8,7 @@ import {getAuth} from "./firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyChBW4hFdfCmT1QCrFvuqEzfaRVe9XfM9w",
-    authDomain: "orgcasas-5c482.firebaseapp.com",
-    projectId: "orgcasas-5c482",
-    storageBucket: "orgcasas-5c482.firebasestorage.app",
-    messagingSenderId: "387368700143",
-    appId: "1:387368700143:web:4d04c9b03510284fe5f31a",
-    measurementId: "G-XDLT7R9DTV"
+    _ _
 };
 
 // Initialize Firebase
